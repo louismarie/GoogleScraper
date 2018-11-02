135f523dd4e3c18fc4a2b533adf6ddc51b941957
louismarie <12646487+louismarie@users.noreply.github.com>
2018-11-02 19:42:32 +0100

    add options

--- GoogleScraper/selenium_mode.py
+++ GoogleScraper/selenium_mode.py
@@ -49,8 +49,10 @@ def check_detection(config, search_engine_name):
     chromedriver = config.get('chromedriver_path', '/usr/bin/chromedriver')
 
     options = webdriver.ChromeOptions()
-    options.add_argument('headless')
+    options.add_argument('--headless')
     options.add_argument('window-size=1200x600')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
 
     browser = webdriver.Chrome(chrome_options=options, executable_path=chromedriver)
 
